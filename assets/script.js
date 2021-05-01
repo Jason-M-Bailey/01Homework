c7c41b3ca9841e0d341e04b3771648fa634a7d77
Jason Bailey <jmbailey816@gmail.com>
2021-05-01 11:30:22 -0400

    Header fully hidden on small screens

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
   } else {
-    document.getElementById("header").style.top = "-70px";
+    document.getElementById("header").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
